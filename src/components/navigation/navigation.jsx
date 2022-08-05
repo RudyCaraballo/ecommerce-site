b46e76346442996f3c36ea7354a6d88a8435cb99
iggyscode <iggydeveloper@protonmail.com>
2022-08-05 18:24:21 -0400

    imported svg as a component bug fixed

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,12 +1,14 @@
 import { Outlet, Link } from "react-router-dom"
 import { Fragment } from "react"
+import Icon  from "../../assets/logo.jsx"
+import './navigation.scss'
 
 export default function Navigation() {
     return(
         <Fragment>
             <div>
                 <Link to="/" className="logo-container">
-                    <div>Logo</div> 
+                    <Icon/>
                 </Link>
                 <div className="links-container">
                     <Link to="/shop">Shop</Link>

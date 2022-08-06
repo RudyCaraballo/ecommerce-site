a8685a3b20b438595c7ecc5f1de3e1485a0ad962
iggyscode <iggydeveloper@protonmail.com>
2022-08-06 14:07:16 -0400

    fixed dotenv/react bug preventing .env vars being read

--- src/routes/sign-in/sign-in.jsx
+++ src/routes/sign-in/sign-in.jsx
@@ -1,5 +1,17 @@
+import { signInWithGooglePopup } from "../../util/firebase"
 
 export default function SignIn(){
-    return (<h1>This is the sign in page</h1>)
+
+    const signIn = async() => {
+        const response = await signInWithGooglePopup();
+        console.log(response);
+    }
+
+    return (
+    <div>
+        <h1>This is the sign in page</h1>
+        <button onClick={signIn}>Sign in With Google</button>
+    </div>
+    )
 
 }
--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,21 +1,35 @@
 import {initializeApp} from 'firebase/app'
-import * as dotenv from 'dotenv' 
-dotenv.config({path: '../../.env'})
+import {
+    getAuth, 
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
+    } from 'firebase/auth'
+
 
  
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAIN,
-    projectId: process.env.PROJECTID,
-    storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID,
-    appId: process.env.APPID
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID
   };
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider()
+
+  provider.setCustomParameters({
+    prompt: "select_account"
+  })
+
+  export const auth = getAuth()
+  export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
 
 

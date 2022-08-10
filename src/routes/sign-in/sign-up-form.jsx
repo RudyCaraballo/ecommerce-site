64b811faa22acc2b12e9b0e1e65d4ebfb4e9eb76
iggyscode <iggydeveloper@protonmail.com>
2022-08-10 13:32:46 -0400

    added useState to save sign in entry

--- src/routes/sign-in/sign-up-form.jsx
+++ src/routes/sign-in/sign-up-form.jsx
@@ -1,20 +1,39 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+}
+
 export default function SignUpForm() {
+    const [formFields, setFormFeilds] = useState(defaultFormFields)
+    const {username, email, password, confirmPassword} = formFields
+    console.log(formFields);
+    
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setFormFeilds({...formFields, [name]: value})
+
+    }
+    
     return (
         <div>
             <h1>Sign up with your email and password</h1>
             <form onSubmit={() => {}}>
 
-                <label>Display Name</label>
-                <input type="text" required />
+                <label>Username</label>
+                <input type="text" name="username" onChange={handleChange} value={username} required />
 
                 <label>Email</label>
-                <input type="email" required />
+                <input type="email" name="email" onChange={handleChange} value={email} required />
 
                 <label>Password</label>
-                <input type="password" required />
+                <input type="password" name="password" onChange={handleChange} value={password} required />
 
                 <label>Confirm Password</label>
-                <input type="password" required />
+                <input type="password" name="confirmPassword" onChange={handleChange} value={confirmPassword} required />
 
                 <button type="submit">Sign up</button>
             </form>

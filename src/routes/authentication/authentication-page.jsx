09d0af6f2146ab6c60a46d73b2833204625e03ee
iggyscode <iggydeveloper@protonmail.com>
2022-09-06 14:05:55 -0400

    added refactoring messages

--- src/routes/authentication/authentication-page.jsx
+++ src/routes/authentication/authentication-page.jsx
@@ -3,6 +3,7 @@ import SignInForm from "../../components/sign-in-form/sign-in-form"
 import './authentication.scss'
 
 export default function Authentication(){
+    //TODO: too big on the page needs to be fixed in stylesheet
     return (
     <div className="authentication-container">
         <SignUpForm /> 
